60db972c71adc21c13cbef2b2157b87a3674c003
Toan vip pro <toanb2012046@student.ctu.edu.vn>
2022-11-08 23:32:03 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const {engine} = require('express-handlebars')
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// HTTP logger
+app.use(morgan('combined'));
+
+
+// Template engine
+app.engine('hbs', engine({extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// method
+//home
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+//news
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
