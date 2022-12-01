05268d3df10535038ecac94085f5e50fd80060d3
Toan vip pro <toanb2012046@student.ctu.edu.vn>
2022-12-01 20:51:30 +0700

    update node-scss, bootstrap5

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
 app.use(morgan('combined'));
-
 
 // Template engine
 app.engine('hbs', engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// method
-//home
+// route
 app.get('/', (req, res) => {
   res.render('home');
 })
